ad176f594befec3f1a5828b25072936306dccdc9
mfranck340 <mfranck340@gmail.com>
2026-03-04 11:46:19 +0100

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://mfranck340.github.io',
   base: '/mfranck340.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), tailwind()] //sitemap()
 });
